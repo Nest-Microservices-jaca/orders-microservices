5a470bf3c1a3540d3f408af5432e7a26db74bbd1
jacapera <capera125@gmail.com>
2024-05-11 18:17:05 -0500

    add-dto create order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller()
 export class OrdersController {
@@ -19,17 +18,13 @@ export class OrdersController {
   }
 
   @MessagePattern('findOneOrder')
-  findOne(@Payload() id: number) {
+  findOne(@Payload('id') id: number) {
     return this.ordersService.findOne(id);
   }
 
-  @MessagePattern('updateOrder')
-  update(@Payload() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(updateOrderDto.id, updateOrderDto);
-  }
-
-  @MessagePattern('removeOrder')
-  remove(@Payload() id: number) {
-    return this.ordersService.remove(id);
+  @MessagePattern('changeOrderStatu')
+  changeOrderStatus() {
+    //return this.ordersService.changeStatus();
+    throw new NotImplementedException();
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersService');
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database conneted');
+  }
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    return createOrderDto;
   }
 
   findAll() {
@@ -14,13 +19,5 @@ export class OrdersService {
 
   findOne(id: number) {
     return `This action returns a #${id} order`;
-  }
-
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
   }
 }
